26db0153509dd9140227a51456b2651e4b496ccb
maria <mariapetrova.pmp@gmail.com>
2022-07-21 14:46:46 +0300

    fix (map)

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ Array.prototype.map= function(callback) {
     let newArray = []
     for (let index=0; index < this.length; index++) {
         let currentValue = this[index]
-        newArray.push(callback.call(currentValue, index, this));
+        newArray.push(callback(currentValue, index, this));
     }
     return  newArray;
 };
 
-let array = [1,2,3, 4, 5]
+let array = [1,2,3,4,5]
 let newArray = array.map( (item, index, arr) =>{
        return (item * 2)
     })
