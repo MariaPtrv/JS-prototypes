5186b31abd4e3c98a162022a22cfcde56abaa4b9
Maria <58878188+MariaPtrv@users.noreply.github.com>
2022-07-26 23:48:35 +0300

    feature (sorts): add selection sort

--- sorts.js
+++ sorts.js
@@ -51,3 +51,19 @@ function insertionSort(array) {
   }
   return array;
 }
+
+
+function selectionSort(array) {
+  for (let i = 0; i < array.length - 1; i++) {
+    let indexMin = i;
+    for (let j = i + 1; j < array.length; j++) {
+        if (array[indexMin] > array[j]) {
+            indexMin = j;
+        }
+    }
+    if (indexMin !== i) {
+        [array[i], array[indexMin]] = [array[indexMin], array[i]];
+    }
+}
+return array;
+}
