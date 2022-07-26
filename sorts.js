6e27ece2bbf87cebafcca9633dbbe2eb6fd48c8f
Maria <58878188+MariaPtrv@users.noreply.github.com>
2022-07-26 23:47:58 +0300

    feature (sorts): add insertion sort

--- sorts.js
+++ sorts.js
@@ -37,3 +37,17 @@ function cocktailSort(array) {
   }
   return array;
 }
+
+
+function insertionSort(array) {
+  for (let i = 1; i < array.length; i++) {
+    const current = array[i];
+    let j = i;
+    while (j > 0 && array[j - 1] > current) {
+      array[j] = array[j - 1];
+      j--;
+    }
+    array[j] = current;
+  }
+  return array;
+}
