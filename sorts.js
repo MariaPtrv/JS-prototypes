a3f87866865cb417c76d1d77be3d9ee2db6652a9
Maria <58878188+MariaPtrv@users.noreply.github.com>
2022-07-26 23:50:37 +0300

    feature (sorts): add merge sort

--- sorts.js
+++ sorts.js
@@ -101,3 +101,25 @@ function quickSort(array, left = 0, right = array.length - 1) {
   }
   return array;
 }
+
+
+function merge(left, right) {
+    let arr = []
+    while (left.length &amp;&amp; right.length) {
+        if (left[0] < right[0]) {
+            arr.push(left.shift())  
+        } else {
+            arr.push(right.shift()) 
+        }
+    }
+    return [ ...arr, ...left, ...right ]
+}
+
+function mergeSort(array) {
+  const half = array.length / 2
+  if(array.length < 2){
+    return array 
+  }
+  const left = array.splice(0, half)
+  return merge(mergeSort(left),mergeSort(array))
+}
