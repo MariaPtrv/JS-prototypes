ec710136d6e1805610b05a24eff8d677ebc87d04
Maria <58878188+MariaPtrv@users.noreply.github.com>
2022-07-26 23:49:42 +0300

    feature (sorts): add quick sort

--- sorts.js
+++ sorts.js
@@ -67,3 +67,37 @@ function selectionSort(array) {
 }
 return array;
 }
+
+function partition(array, left, right) {
+  let pivot = array[Math.floor((right + left) / 2)],
+    i = left,
+    j = right;
+  while (i <= j) {
+    while (array[i] < pivot) {
+      i++;
+    }
+    while (array[j] > pivot) {
+      j--;
+    }
+    if (i <= j) {
+      swap(array, i, j);
+      i++;
+      j--;
+    }
+  }
+  return i;
+}
+
+function quickSort(array, left = 0, right = array.length - 1) {
+  let index;
+  if (array.length > 1) {
+    index = partition(array, left, right);
+    if (left < index - 1) {
+      quickSort(array, left, index - 1);
+    }
+    if (index < right) {
+      quickSort(array, index, right);
+    }
+  }
+  return array;
+}
